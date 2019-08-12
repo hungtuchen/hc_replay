94fdd4e5942d96d5053d4c91b7b5ee5855cd1708
Matthew Schafer <41482688+matty-gee@users.noreply.github.com>
2019-08-12 13:53:26 -0400

    Add files via upload
    Newer newer

--- simuluationCode/behaviorGenerator.m
+++ simuluationCode/behaviorGenerator.m
@@ -1,36 +1,55 @@
-function xy_coords = behaviorGenerator(x_range, duration, forward_prob) 
+function locations = behaviorGenerator2D(type, length, duration, forward_prob) 
     % MIND 2019: written by matt schafer
     % to generate locations within a maze (x,y coords)
     
-    x = 1; %y = round(y_range/2); % starting point
-    xy_coords = zeros(1,duration); % to output the location coords    
-    if nargin == 2 
-        forward_prob = .8; % pr(move forward in maze)
+    switch type 
+        case 'linear' 
+            location = 1;
+        case 'square'
+            location = [1; length/2];
     end
     
-    for t = 1 : duration
+    locations = location; 
+    
+    if nargin == 3 
+        forward_prob = .5; % pr(move forward in maze);
+    end
+    
+    for t = 1 : duration 
         
-        speed = randi(5); % want to make into a distribution
-                     
-        if rand > (1 - forward_prob)
-            x = x + 1 * speed; % move forward
+        speed = randi(3); 
+        move = 1 * speed; 
+        
+        if type == 'square'
+           dim = randi(2);
         else
-            x = x - 1 * speed; % move backward
-        end            
-   
-        if x <= 1
-            x = 1;
+            dim = 1;
         end
         
-        if x >= x_range
-            x = x_range;
-            x = x - 1 * speed; % move backward
+        if rand > (1 - forward_prob)
+            location(dim) = location(dim) + move;
+        else
+            location(dim) = location(dim) - move;
         end
         
-        xy_coords(1,t) = x; % log coordinates
+        % respect the bounardies
+        if location(dim) <= 0
+            location(dim) = 0;
+        end
+        
+        if location(dim) >= length
+            location(dim) = length - move; % move backward
+        end
+
+        locations = [locations, location];
         
     end
     
-    plot(xy_coords(1,:)); % plot
+    % plot
+    if type == 'square'
+        plot(locations(1,:), locations(2,:)); 
+    else
+        plot(locations(1,:));
+    end
     
 end
