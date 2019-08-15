cd78701d6c2ff2fffa77f65ec7e37a298b8b0126
lemuelle <lemuelle@buffalo.edu>
2019-08-15 18:26:13 -0400

    Add a new version of theta event times

--- theta_interval/theta_times.m
+++ theta_interval/theta_times.m
@@ -1,31 +1,31 @@
-function [intervals] = theta_times(cfg, csc)
-%purpose: get theta cycle times
-%Inputs: configurations(cfg) and lfp data(csc)from MVDM
-%Outputs: time intervals between peaks
-​
-%% output
-intervals = [];
-​
-%% load necessary info
+%purpose: script to get theta cycle times
+%Outputs: time intervals between peaks, tstart and tstop
+
+
+%load necessary info
+pos = LoadPos([]); % note, this is raw position data read from the .nvt file, units are "camera pixels"
+LoadExpKeys; % see https://github.com/mvdm/vandermeerlab/blob/master/doc/HOWTO_ExpKeys_Metadata.md
+LoadMetadata;
+
+cfg = []; cfg.fc = ExpKeys.goodTheta(1);
 lfp = LoadCSC(cfg);
-pos = LoadPos([]);
-LoadExpKeys;
-​
+
 %% filter LFP to get theta band
+
 lfp_tsd = FilterLFP(cfg,lfp);
-​
-​
+
+
 %% restrict to times when rat is running
-​
+
 pos = restrict(pos,ExpKeys.TimeOnTrack,ExpKeys.TimeOffTrack);%position
 spd = getLinSpd([],pos); % get speed (in "camera pixels per second")
-​
+
 cfg_spd = []; cfg_spd.method = 'raw'; cfg_spd.threshold = 10; %config speed
 run_iv = TSDtoIV(cfg_spd,spd); % running intervals with speed above 10 pix/s
-​
+
 %restrict theta to run_iv
 lfp_tsd = restrict(lfp_tsd,run_iv.tstart,run_iv.tend);
-​
+
 %% get power of signal, to do in future
 % lfp_tsd = LFPpower([],lfp_tsd); %envelope is default?
 % lfp_tsd_zpwr = zscore_tsd(lfp_tsd);%z score
@@ -35,24 +35,22 @@ lfp_tsd = restrict(lfp_tsd,run_iv.tstart,run_iv.tend);
 % cfg.method = 'raw'; % first normalize the data
 % cfg.threshold = 0;%3?
 % cfg.dcn = '>'; % detect intervals with z-score lower than threshold
-​
+
 %% get times of peaks
-​
+
 [peaks,locs] = findpeaks(lfp_tsd.data);
 peak_times = lfp_tsd.tvec(locs);%finds peak tsd
 peak_iv = diff(peak_times);
-​
-​
+
+
 %% create peak intervals
-​
+
 tstart = peak_iv(1:end-1);
 tstop = nan(length(peak_iv),1);
-​
+
 for i = 1:length(peak_iv)-1;
     tstop(i)= peak_iv(i+1);
 end
 tstop = tstop(1:end-1);
-​
-%package
-intervals.tstart = tstart;
-intervals.tstop = tstop;
+
+
