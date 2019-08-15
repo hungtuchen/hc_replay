c3ad0868381c093a7d8bdd79be8c9bff33ddb0fa
Devlaxman93 <sdevlaxman@yahoo.co.in>
2019-08-14 21:03:37 -0400

    SWR IV data from LFP with the updated code

--- SWRs/LoadSWRs.m
+++ SWRs/LoadSWRs.m
@@ -1,3 +1,21 @@
+function SWR_iv = LoadSWRs(hc_replay_path)
 % Load SWRs
-addpath(genpath('C:\Users\sdevl\Desktop\IMP\Important\Dartmouth summer school\hc_replay-master\hc_replay-master\SWRs'));
-load('SWR.mat');
+% Input:
+% hc_replay_path -> Should be the path of our hc_replay github folder
+
+%Output:
+% iv file with SWR time events 
+
+    current_path = pwd;
+    if ispc
+        filesep = '\';
+    elseif ismac
+        filesep = '/';
+    else
+        filesep = '/';
+    end
+    SWR_path = cat(2, hc_replay_path, filesep, 'SWRs');
+    cd(SWR_path)
+    SWR_iv = load('R064-2015-04-22_SWRiv.mat'); %insert the iv struct here
+    cd(current_path)
+end
