da15cc57ab09dfd42f0f1caeb6bea2c978fbeb15
Matthew Schafer <matthew.schafer@icahn.mssm.edu>
2019-08-14 20:52:26 -0400

    small comments for ms

--- simuluationCode/genSpikeMatrix.m
+++ simuluationCode/genSpikeMatrix.m
@@ -4,28 +4,30 @@ function [locations, fieldVector, fields, spikeMatrix, positions] = ...
 % firingRateVector: for every place field, the firing rate - the
     % probability of firing (vector length: nfields)
 % fieldWidthVector: how big do you want the field to be? (vector length: nfields)
-% arenaSize: 
 
-% example: genSpikeMatrix(nfields,nfields,firingRateVector,fieldWidthVector,arenaSize,timeSteps)
+% EXAMPLE
+% nfields = 35;
+% firingRateVector = ones(31,1); 
+% fieldWidthVector = ones(31,1);
+% arenaSize = 100;
+% timeSteps = 1000;
+
 %%
-% nfields = 15;
-% arenaSize = 50;
-
 fields = gen2dfields(arenaSize,nfields,fieldWidthVector);
 fieldVector = reshape(fields,[],nfields); 
 % to unwrap: reshape(fieldVector, [arenaSize arenaSize nfields])
+
 %%
 [locations, arena] = behaviorGenerator('square',arenaSize,timeSteps,0,0);
 
 %%
-
 [spikeMatrix, positions] = simulateSpikes(locations,arena,fieldVector,firingRateVector,'homeBrew');
 
-%%
 %%
 quickPlotRaster(spikeMatrix)
 [maxSpikes,spikiestCell] = max(sum(spikeMatrix,1));
 quickPlotSpikePosition2D(locations,spikeMatrix,[spikiestCell],arenaSize)
 disp([spikiestCell maxSpikes]);
 
+% format data for decoder
 packData(positions, locations, spikeMatrix, fieldVector, arenaSize);
